66bbbe71244d1a93c40fc9af96da8e5547785f83
aiegoo <eozz21@gmail.com>
2019-03-05 15:12:55 +0900

    add exercise files js html

---
+++ js/ex1.js
@@ -0,0 +1,13 @@
+document.getElementById("#bt1").addEventListener("click", function(){
+  alert("Hello Real World");
+  document.getElementById("cmt").innerHTML = "현실세계를 선택!";  
+});
+
+$bt1("#bt2").click(function(){
+  alert("Hello Vitual World");
+  $bt1("#cmt").html("가상현실을 선택!");
+});
+
+var a;
+a=10;
+console.log(a);
